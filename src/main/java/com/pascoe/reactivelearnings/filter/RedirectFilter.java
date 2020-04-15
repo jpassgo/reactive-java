d13efe9b35020d55e597564c6ba766ed228c52b2
jpassgo <jeffpascoe9@gmail.com>
2020-04-14 21:26:51 -0500

    Trying to chain WebClient requests together.

--- src/main/java/com/pascoe/reactivelearnings/filter/RedirectFilter.java
+++ src/main/java/com/pascoe/reactivelearnings/filter/RedirectFilter.java
@@ -2,9 +2,12 @@ package com.pascoe.reactivelearnings.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import javax.xml.ws.ServiceMode;
 
 import static org.springframework.web.reactive.function.client.WebClient.*;
 
@@ -14,12 +17,17 @@ public class RedirectFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+        Mono<ResponseSpec> initialExchange = buildExchange(exchange);
+        Mono<ResponseSpec> onErrorExchange = buildExchange(exchange);
+        return initialExchange
+                .onErrorResume((error) -> {
+                    return onErrorExchange;
+                })
+                .then(chain.filter(exchange));
     }
 
     Mono<ResponseSpec> buildExchange(ServerWebExchange exchange) {
-        webClient.get()
-
+        return Mono.error(Exception::new);
     }
 
 }
